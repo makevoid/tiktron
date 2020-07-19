10c21dd01d35c4fb36320d8d6d07af4307ac3dd9
makevoid <makevoid@gmail.com>
2020-07-19 11:36:51 +0100

    add ipc code

--- main.js
+++ main.js
@@ -3,18 +3,90 @@ const path = require('path')
 const EventEmitter = require('events')
 const tt = require('tiktok-scraper')
 
-const ttOptions = { number: 1, noWaterMark: true }
-
 class MainEvents extends EventEmitter {}
 
 const mainEvents = new MainEvents()
-mainEvents.on('event', () => {
-  console.log('an event occurred!')
-})
 
-const hashtag = "doge"
-const username = "makevoid"
+const ttOptions = { number: 1, noWaterMark: true }
 
+const loadVideo = () => {
+  console.log("loading video")
+}
+
+const transition = () => {
+  console.log("starting css animation to replace video")
+}
+
+const actions = {
+  nextVideo: (evt) => {
+    loadVideo()
+    transition({ direction: "next" })
+  },
+  prevVideo: (evt) => {
+    loadVideo()
+    transition({ direction: "prev" })
+  },
+}
+
+const userEvent = ({ user }) => {
+  console.log("user:", user)
+}
+
+const bindMainEvents = () => {
+  // bind user events
+  mainEvents.on('ev-user', ({ user }) => userEvent({ user }))
+}
+
+const bindMainButtons = ({ buttons }) => {
+  console.log(buttons)
+  buttons.prevButton.addEventListener(actions.prevVideo)
+  buttons.nextButton.addEventListener(actions.nextVideo)
+}
+
+const bindVideoTags = () => {
+  var vidMain = document.querySelector("video.video-main")
+  var vidNext = document.querySelector("video.video-next")
+  var vidPrev = document.querySelector("video.video-prev")
+  vid.volume = 0 // muted
+  // vid.volume = 1 // 100%
+}
+
+
+const bindIPC = () => {
+  const ipc = require('electron').ipcMain
+
+  ipc.on('invokeAction', (event, data) => {
+    const result = processData(data)
+    event.sender.send('actionReply', result)
+  })
+}
+
+// main
+
+const main = () => {
+  bindIPC()
+
+  ;(async () => {
+
+    const hashtag = "doge"
+    const username = "makevoid"
+    // const results = await tt.hashtag(hashtag, ttOptions)
+    const result = await tt.user(username, ttOptions)
+    const users = result.collector
+    const user = users[0]
+
+    mainEvents.emit('ev-user', { antani: "true" })
+    // console.log(Object.keys(user))
+    const { id, name, nickname, avatar, covers, videoUrl, webVideoUrl, playCount, shareCount } = user
+    const { default: defaultCover } = covers
+    console.log(`id: ${id}`)
+    console.log(`nickname: ${nickname}`)
+    console.log(`webVideoUrl: ${videoUrl}`)
+    // console.log(user.id)
+  })()
+}
+
+// electron window management
 
 const createWindow = () => {
   const mainWindow = new BrowserWindow({
@@ -28,6 +100,8 @@ const createWindow = () => {
   mainWindow.loadFile('index.html')
 
   mainWindow.webContents.openDevTools()
+
+  main()
 }
 
 app.whenReady().then(() => {
@@ -42,14 +116,3 @@ app.whenReady().then(() => {
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') app.quit()
 })
-
-console.log(">")
-
-;(async () => {
-  // const results = await tt.hashtag(hashtag, ttOptions)
-  const results = await tt.user(username, ttOptions)
-
-  console.log("results:", results.collector)
-
-  mainEvents.emit('event')
-})()
--- src/scrape.js
+++ src/scrape.js
@@ -12,28 +12,32 @@ const userEvent = ({ user }) => {
   console.log("user:", user)
 }
 
-const bindMainEvents = () => {
-  // bind user events
-  mainEvents.on('ev-user', ({ user }) => userEvent({ user }))
+
+const loadVideo = ({ position }) => {
+  console.log("loading video")
+  if (!position) throw new Error("Position not specified - position must be `prev` or `next`")
+
+  // send event
 }
 
-const main = () => {
-  bindMainEvents()
 
+const main = () => {
   ;(async () => {
 
     const hashtag = "doge"
     const username = "makevoid"
     // const results = await tt.hashtag(hashtag, ttOptions)
-    const collection = await tt.user(username, ttOptions)
-    const users = results.collector
+    const result = await tt.user(username, ttOptions)
+    const users = result.collector
     const user = users[0]
 
     mainEvents.emit('ev-user', { antani: "true" })
-    console.log(Object.keys(user))
+    // console.log(Object.keys(user))
     const { id, name, nickname, avatar, covers, videoUrl, webVideoUrl, playCount, shareCount } = user
-    const { defaultCover: default } = covers
-    // console.log(`nickname: ${nickname}`)
+    const { default: defaultCover } = covers
+    console.log(`id: ${id}`)
+    console.log(`nickname: ${nickname}`)
+    console.log(`webVideoUrl: ${videoUrl}`)
     // console.log(user.id)
   })()
 }
